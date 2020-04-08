b814d45625f612212c9934513e83204e815a7df0
Travis Lingenfelder <trlingen@microsoft.com>
2020-04-08 13:41:32 -0500

    initial checkin

---
+++ PnP.Provisioning/DOTNET/SP.PnP.Provisioning.Samples/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Identity.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.PnP.Templates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            string sourceSiteUrl = args[0];
+            string destSiteUrl = args[1];
+
+            if (string.IsNullOrEmpty(sourceSiteUrl))
+                throw new ArgumentNullException("argument1");
+
+            if (string.IsNullOrEmpty(sourceSiteUrl))
+                throw new ArgumentNullException("argument2");
+
+
+            var certificate = new X509Certificate2(System.IO.Path.Combine(AppContext.BaseDirectory, ConfigurationManager.AppSettings["pfxPath"] as string), ConfigurationManager.AppSettings["pfxPass"] as string);
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder
+                .Create(ConfigurationManager.AppSettings["clientId"] as string)
+                .WithCertificate(certificate)
+                .WithTenantId(ConfigurationManager.AppSettings["tenantId"] as string)
+                .Build();
+
+            Uri sourceUri = new Uri(sourceSiteUrl);
+            AuthenticationResult auth = await app.AcquireTokenForClient(new string[] { $"https://{sourceUri.Host}/.default" }).ExecuteAsync();
+
+            string resourceFolder;
+            ProvisioningTemplate template = ProvisioningHelper.GetProvisioningTemplate(sourceSiteUrl, auth.AccessToken, out resourceFolder);
+            ProvisioningHelper.ApplyProvisioningTemplate(destSiteUrl, template, resourceFolder, auth.AccessToken);
+        }
+    }
+}
---
+++ PnP.Provisioning/DOTNET/SP.PnP.Provisioning.Samples/ProvisioningHelper.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SP.PnP.Templates
+{
+    public class ProvisioningHelper
+    {
+        public static ProvisioningTemplate GetProvisioningTemplate(string webUrl, string authToken, out string resourceFolder)
+        {
+            ConsoleColor defaultForeground = Console.ForegroundColor;
+
+            AuthenticationManager authManager = new AuthenticationManager();
+            using (var ctx = authManager.GetAzureADAccessTokenAuthenticatedContext(webUrl, authToken))
+            {
+                ctx.RequestTimeout = Timeout.Infinite;
+
+                // Just to output the site details
+                Web web = ctx.Web;
+                ctx.Load(web, w => w.Title);
+                ctx.ExecuteQueryRetry();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Your site title is:" + ctx.Web.Title);
+                Console.ForegroundColor = defaultForeground;
+
+                ProvisioningTemplateCreationInformation ptci = new ProvisioningTemplateCreationInformation(ctx.Web);
+
+                // Create FileSystemConnector to store a temporary copy of the template 
+                Guid templateId = Guid.NewGuid();
+                resourceFolder = Path.Combine(Path.GetTempPath(), templateId.ToString());
+                ptci.FileConnector = new FileSystemConnector(resourceFolder, "");
+                ptci.ProgressDelegate = delegate (String message, Int32 progress, Int32 total)
+                {
+                    // Only to output progress for console UI
+                    Console.WriteLine("{0:00}/{1:00} - {2}", progress, total, message);
+                };
+
+                // Execute actual extraction of the template
+                ProvisioningTemplate template = ctx.Web.GetProvisioningTemplate(ptci);
+
+                // We can serialize this template to save and reuse it
+                // Optional step 
+                XMLTemplateProvider provider = new XMLFileSystemTemplateProvider(resourceFolder, "");
+                Console.ForegroundColor = ConsoleColor.White;
+                provider.SaveAs(template, $"{templateId.ToString()}.xml");
+                Console.WriteLine($"Template saved to {resourceFolder}\\{templateId}.xml");
+                Console.ForegroundColor = defaultForeground;
+
+                return template;
+            }
+        }
+
+        public static void ApplyProvisioningTemplate(string webUrl, ProvisioningTemplate template, string resourcePath, string authToken)
+        {
+            AuthenticationManager authManager = new AuthenticationManager();
+            using (var ctx = authManager.GetAzureADAccessTokenAuthenticatedContext(webUrl, authToken))
+            {
+                ConsoleColor defaultForeground = Console.ForegroundColor;
+
+                ctx.RequestTimeout = Timeout.Infinite;
+
+                // Just to output the site details
+                Web web = ctx.Web;
+                ctx.Load(web, w => w.Title);
+                ctx.ExecuteQueryRetry();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Your site title is:" + ctx.Web.Title);
+                Console.ForegroundColor = defaultForeground;
+
+                // We could potentially also upload the template from file system, but we at least need this for branding file
+                //XMLTemplateProvider provider =
+                //       new XMLFileSystemTemplateProvider(@"c:\temp\pnpprovisioningdemo", "");
+                //template = provider.GetTemplate("PnPProvisioningDemo.xml");
+
+                ProvisioningTemplateApplyingInformation ptai = new ProvisioningTemplateApplyingInformation();
+                ptai.ProgressDelegate = delegate (String message, Int32 progress, Int32 total)
+                {
+                    Console.WriteLine("{0:00}/{1:00} - {2}", progress, total, message);
+                };
+
+                // Associate file connector for assets
+                FileSystemConnector connector = new FileSystemConnector(resourcePath, "");
+                template.Connector = connector;
+
+                // Since template is actual object, we can modify this using code as needed
+                //template.Lists.Add(new ListInstance()
+                //{
+                //    Title = "PnP Sample Contacts",
+                //    Url = "lists/PnPContacts",
+                //    TemplateType = (Int32)ListTemplateType.Contacts,
+                //    EnableAttachments = true
+                //});
+
+                web.ApplyProvisioningTemplate(template, ptai);
+            }
+        }
+    }
+}
